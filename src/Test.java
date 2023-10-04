5cf8cd2f7caef13306cb79f4574b7f7f7f614d4a
Baver Yurtdas <baveryurtdas@gmail.com>
2023-10-04 13:34:09 +0300

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir değer giriniz");
+        double b =input.nextDouble();
+        System.out.println(b);
+    }
+
+}
