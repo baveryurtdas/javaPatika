4acf551b2fdc263cb0b0875049801cb1bd90350d
Baver Yurtdas <baveryurtdas@gmail.com>
2023-10-05 17:51:28 +0300

    pratik eklendi

---
+++ src/sortingNumbersFromLargeToSmall.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class sortingNumbersFromLargeToSmall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("First Number : ");
+        a = input.nextInt();
+        System.out.print("Second Number : ");
+        b = input.nextInt();
+        System.out.print("Third Number : ");
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println("a > b > c");
+            } else {
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println("b > a > c");
+            } else {
+                System.out.println("b > c > a");
+            }
+        } else if ((c > a) && (c > b)) {
+            if (a > b) {
+                System.out.println("c > a > b");
+            } else {
+                System.out.println("c > b > a");
+            }
+        }
+    }
+}
