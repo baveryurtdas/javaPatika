712aea125dd0294517c0e222921ae8367fccf7ec
Baver Yurtdas <baveryurtdas@gmail.com>
2023-10-06 15:38:10 +0300

    yeni pratik eklendi

---
+++ src/findingAnEvenNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class findingAnEvenNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, sum = 0, count = 0;
+        double avarage;
+        System.out.print("Enter a number : ");
+        n = input.nextInt();
+
+        //with for cycle
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                sum += i;
+                count++;
+
+            }
+
+        }
+        if (count > 0) {
+            avarage = (double) sum / count;
+            System.out.println("0'dan " + n + "'e kadar olan 3 ve 4'e tam bölünen sayıların ortalaması = " + avarage);
+        } else {
+            System.out.println("0'dan " + n + "'e kadar olan 3 ve 4'e tam bölünen bir sayı yok.");
+        }
+    }
+}
