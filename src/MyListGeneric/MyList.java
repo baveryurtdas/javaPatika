857546708a1b7ba68da0861e383d74a2b130fb79
Baver Yurtdas <baveryurtdas@gmail.com>
2023-11-23 12:16:49 +0300

    generic liste ödevi tamamlandi

--- src/MyListGeneric/MyList.java
+++ src/MyListGeneric/MyList.java
@@ -3,152 +3,153 @@ package MyListGeneric;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class MyList<T> {
-    int capacity;
-    T[] list;
-    T[] listTemp;
-    int listSize = 0;
+public class MyList<T> implements IMyList<T> {
+    private int size = 0;
+    private int capacity;
+    T[] arr;
 
+
+    // Varsayılan constructor, kapasiteyi 10 olarak ayarlar ve diziyi oluşturur.
     public MyList() {
         this.capacity = 10;
+        this.arr = (T[]) new Object[capacity];
     }
+
+    // Belirtilen kapasiteye sahip constructor
 
     public MyList(int capacity) {
         this.capacity = capacity;
+        this.arr = (T[]) new Object[capacity];
     }
 
-
-
-    public void pCapacity() {
-        this.listTemp = (T[]) new Object[this.capacity];
-        for (int i = 0; i < this.list.length; i++) {
-            this.listTemp[i] = this.list[i];
-        }
+    // Listenin mevcut boyutunu döndürür
+    @Override
+    public int size() {
+        return size;
     }
 
+    // Listenin kapasitesini döndürür
+    @Override
     public int getCapacity() {
         return capacity;
     }
 
-    public int size() {
-        return this.listSize;
+    // Eleman ekler, kapasite yetersizse iki katına çıkarır
+    @Override
+    public void add(T data) {
+        if (size == capacity) {
+            int newCapacity = capacity * 2;
+            arr = Arrays.copyOf(arr, newCapacity);
+            capacity = newCapacity;
+        }
+        arr[size++] = data;
     }
 
-    public void sizeC() {
-        int a = 0;
-        for (int i = 0; i < this.list.length; i++) {
-            if (this.list[i] != null) {
-                a++;
+    // Belirtilen indeksteki elemanı döndürür
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+        return arr[index];
+    }
+
+    // Belirtilen indeksteki elemanı kaldırır
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) return null;
+        T removedItem = arr[index];
+        arr[index] = null;
+        for (int i = index + 1; i < size; i++) {
+            if (arr[i] != null) {
+                arr[i - 1] = arr[i];
+                arr[i] = null;
             }
         }
-        this.listSize = a;
+        size--;
+        return removedItem;
     }
 
-    public void add(T data) {
-        if (this.size() < this.capacity) {
-            list[this.listSize] = data;
-            this.sizeC();
-        } else {
-            this.capacity = this.capacity * 2;
-            this.list = Arrays.copyOf(list, this.capacity);
-            list[this.listSize] = data;
-            this.sizeC();
-
-        }
+    // Belirtilen indeksteki elemanı değiştirir
+    @Override
+    public T set(int index, T data) {
+        if (index < 0 || index >= size) return null;
+        arr[index] = data;
+        return data;
     }
 
+    // Listeyi string olarak temsil eden metot
     @Override
     public String toString() {
-        System.out.println("[");
-        for (T x : list) {
-            if (x != null) {
-                System.out.println(x + ",");
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(arr[i]);
+            if (i != size - 1) {
+                sb.append(",");
             }
         }
-        System.out.println("]");
-        return "";
+        sb.append("]");
+        return sb.toString();
     }
 
-    public T get(int index) {
-        T x = this.list[index];
-        return x;
-    }
+    // Belirtilen elemanın ilk indeksini döndürür
 
-    public void remove(int index) {
-        this.listSize--;
-    }
-
-    public void replaceForRemove(int index) {
-        T[] listTemp2 = this.list;
-        this.list[index] = null;
-        for (int i = 0; i < listTemp2.length - 1; i++) {
-            if (index == 0) {
-                this.list[i] = listTemp2[i + 1];
-            } else if (i < index) {
-                this.list[i] = listTemp2[i];
-            } else if (i > index) {
-                this.list[i] = listTemp2[i + 1];
-            }
-        }
-    }
-
-    public void set(int index, T data) {
-        this.list[index] = data;
-    }
-
+    @Override
     public int indexOf(T data) {
-        for (int i = 0; i < this.list.length; i++) {
-            if (data == this.list[i]) {
-                return i;
-            }
+        for (int i = 0; i < size; i++) {
+            if (data.equals(arr[i])) return i;
         }
         return -1;
     }
 
+    // Belirtilen elemanın son indeksini döndürür
+
+    @Override
     public int lastIndexOf(T data) {
-        for (int i = this.list.length - 1; i >= 0; i--) {
-            if (list[i] == data) {
-                return i;
-            }
+        for (int i = size - 1; i >= 0; i--) {
+            if (data.equals(arr[i])) return i;
         }
-
         return -1;
     }
 
+    // Liste boş mu kontrolü
+    @Override
     public boolean isEmpty() {
-        if (this.size() == 0)
-            return true;
-        else return false;
+        return size == 0;
     }
 
-    public Object[] toArray() {
-        Object[] tempObject = new Object[this.list.length];
-        for (int i = 0; i < this.list.length; i++) {
-            tempObject[i] = this.list[i];
+    // Listeyi bir dizi olarak döndürür
+
+    @Override
+    public T[] toArray() {
+        return (T[]) Arrays.copyOf(arr, size);
+    }
+
+    // Listeyi temizler (elemanları null yapar ve boyutu sıfırlar)
+    @Override
+    public void clear() {
+        Arrays.fill(arr, null);
+        size = 0;
+    }
+
+    // Belirtilen aralıktaki alt liste
+    @Override
+    public MyList<T> subList(int start, int finish) {
+        if (start < 0 || finish >= size || start > finish) {
+            throw new IndexOutOfBoundsException();
         }
-        return tempObject;
-    }
-
-    public MyList<T> subList(int s, int f) {
-        MyList<T> sub = new MyList<>(f - s + 1);
-        for (int i = s; i <= f; i++) {
-            sub.add((T) this.list[i]);
+        MyList<T> subList = new MyList<>(finish - start + 1);
+        for (int i = start; i <= finish; i++) {
+            subList.add(get(i));
         }
-        return sub;
+        return subList;
     }
 
+    // Belirtilen elemanın listede olup olmadığını kontrol eder
+    @Override
     public boolean contains(T data) {
-        for (int i = 0; i < this.list.length; i++) {
-            if (data == this.list[i]) {
-                return true;
-            }
+        for (int i = 0; i < size; i++) {
+            if (data.equals(arr[i])) return true;
         }
         return false;
     }
 
-    public void clear() {
-        for (int i = 0; i < this.list.length; i++) {
-            this.list[i] = null;
-        }
-    }
 }
